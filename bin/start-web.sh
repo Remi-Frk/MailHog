0ce54699e8c593bcf39ee956b85bc78b4651e698
Bocquelet Matthias <matthias.bocquelet@sg.social.gouv.fr>
2023-04-24 10:05:07 +0200

    test avec exemple de config donné par MailHog

--- bin/start-web.sh
+++ bin/start-web.sh
@@ -2,4 +2,5 @@
 
 echo "Démarrage et binding sur $PORT"
 
-MailHog -api-bind-addr 0.0.0.0:$PORT -ui-bind-addr 0.0.0.0:$PORT -auth-file=config/config-auth
+# MailHog -api-bind-addr 0.0.0.0:$PORT -ui-bind-addr 0.0.0.0:$PORT -auth-file=config/config-auth
+MailHog -api-bind-addr 0.0.0.0:$PORT -ui-bind-addr 0.0.0.0:$PORT -auth-file=docs/example-auth
